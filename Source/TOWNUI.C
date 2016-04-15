5b2a1dfcec7691ccb40a3c66fcfb37c61761b57e
peewee <cabbruzz@gmail.com>
2016-04-14 21:05:41 -0400

    Fixed crash caused by updating the chat box when it was not active when completing a quest.

--- Source/TOWNUI.C
+++ Source/TOWNUI.C
@@ -1024,7 +1024,8 @@ E_Boolean TownUIFinishedQuest(T_word16 multiplayerStatus, T_byte8 numPlayers, T_
 			//Completed quest has been reported by all players
 			//	Reset them and show them in town
 			PeopleHereReset();
-			TownRedrawChatList();
+			if (IsTownHallChatActive())
+				TownRedrawChatList();
 
 		}
 	}
